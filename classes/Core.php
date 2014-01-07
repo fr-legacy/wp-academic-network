30fd8a194c856f6d9b34cf311294c356a49f1c8f
barryhughes <barry@codingkills.me>
2014-01-07 11:19:21 -0800

    Core tweaks

--- classes/Core.php
+++ classes/Core.php
@@ -2,13 +2,31 @@
 namespace WPAN;
 
 
-class Core {
+class Core
+{
 	/**
 	 * Container for the Core WPAN object.
 	 *
 	 * @var Core
 	 */
 	protected static $object;
+
+	/**
+	 * @var Users
+	 */
+	protected $users;
+
+	/**
+	 * @var Network
+	 */
+	protected $network;
+
+	/**
+	 * Indicates if initial setup is complete. Internal flag.
+	 *
+	 * @var bool
+	 */
+	protected $setup = false;
 
 
 	public static function object() {
@@ -19,12 +37,45 @@ class Core {
 
 
 	/**
-	 * Loads rest of the plugin, sets up key services.
+	 * Marked protected to enforce a singleton pattern.
 	 */
-	protected function __construct() {
-		if ( Network::is_hub() ) Hub\Manager::object();
-		Users\Manager::object();
-		Requests\Manager::object();
-		$result = Network::owned_by( 1 );
+	protected function __construct() {}
+
+	/**
+	 * Sets up and keeps references to other key components.
+	 */
+	public function setup() {
+		if ( $this->setup ) return;
+		else $this->setup = true;
+
+		$this->users = new Users;
+		$this->network = new Network;
+
+		add_action( 'init', array( $this, 'working_tests' ) );
+	}
+
+	/**
+	 * @return Users
+	 */
+	public function users() {
+		return $this->users;
+	}
+
+	/**
+	 * @return Network
+	 */
+	public function network() {
+		return $this->network;
+	}
+
+
+	/**
+	 * Notes ...
+	 *
+	 * Seems that the network.create_teacher_blog() method would best be run via ajax during
+	 * import to avoid a bizarre but non critical visual effect post-site creation.
+	 */
+	public function working_tests() {
+		$this->users()->get_academic_role( 555 );
 	}
 }
