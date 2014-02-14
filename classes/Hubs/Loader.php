05abcfc1529182073270e4418d858211714e66d2
barryhughes <barry@codingkills.me>
2014-02-14 05:57:12 -0800

    Support loading the student hub

--- classes/Hubs/Loader.php
+++ classes/Hubs/Loader.php
@@ -3,6 +3,7 @@ namespace WPAN\Hubs;
 
 use WPAN\Core,
 	WPAN\Hubs\Network\Manager as NetworkHub,
+	WPAN\Hubs\Student\Manager as StudentHub,
 	WPAN\Hubs\Teacher\Manager as TeacherHub,
 	WPAN\Network,
 	WPAN\Users;
@@ -38,6 +39,7 @@ class Loader {
 
 		if ( $this->is_network_hub() ) $this->hub_object = new NetworkHub();
 		if ( $this->is_teacher_hub() ) $this->hub_object = new TeacherHub();
+		if ( $this->is_student_hub() ) $this->hub_object = new StudentHub();
 	}
 
 	/**
@@ -54,8 +56,8 @@ class Loader {
 	}
 
 	/**
-	 * Checks if the current admin environment is a teacher blog and the current user is
-	 * both teacher and this is indeed their primary blog.
+	 * Checks if the current admin environment is a teacher blog and that the current user is
+	 * both a teacher and this is indeed their primary blog.
 	 *
 	 * @return bool
 	 */
@@ -66,6 +68,24 @@ class Loader {
 		if ( ! is_admin() ) return false;
 		if ( ! $this->network->is_teacher_blog( $blog_id ) ) return false;
 		if ( ! $this->users->is_teacher( $user_id ) ) return false;
+		if ( $this->network->get_primary_blog( $user_id ) !== $blog_id ) return false;
+
+		return true;
+	}
+
+	/**
+	 * Checks if the current admin environment is a student blog and that the current user is
+	 * both a student and this is indeed their primary blog.
+	 *
+	 * @return bool
+	 */
+	protected function is_student_hub() {
+		$blog_id = get_current_blog_id();
+		$user_id = wp_get_current_user()->ID;
+
+		if ( ! is_admin() ) return false;
+		if ( ! $this->network->is_student_blog( $blog_id ) ) return false;
+		if ( ! $this->users->is_student( $user_id ) ) return false;
 		if ( $this->network->get_primary_blog( $user_id ) !== $blog_id ) return false;
 
 		return true;
